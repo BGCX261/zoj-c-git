b3d8e7b14231c8087b4aeac2f89ae624a968ea40
jeffrey <zhenjili@gmail.com>
2012-09-05 20:43:26 -0700

    12

---
+++ ZOJ_c++/src/hello.cpp
@@ -0,0 +1,18 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: Sep 5, 2012
+ *      Author: jeffrey
+ */
+
+#include "hello.h"
+
+hello::hello() {
+	// TODO Auto-generated constructor stub
+
+}
+
+hello::~hello() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ZOJ_c++/src/hello.h
@@ -0,0 +1,17 @@
+/*
+ * hello.h
+ *
+ *  Created on: Sep 5, 2012
+ *      Author: jeffrey
+ */
+
+#ifndef HELLO_H_
+#define HELLO_H_
+
+class hello {
+public:
+	hello();
+	virtual ~hello();
+};
+
+#endif /* HELLO_H_ */
